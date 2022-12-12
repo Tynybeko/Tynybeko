f6d74b6f2dc234ead2beee4886592cbf3278553b
Tynybek <tynybeko1122@gmail.com>
2022-12-12 23:44:54 +0600

    js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+let home = document.getElementById('home')
 let aboutme = document.getElementById('aboutme')
 let skills = document.getElementById('skills')
 let portfolio = document.getElementById('portfolio')
 let contacts = document.getElementById('contacts')
 
 
-let home_link = document.getElementById('aboutme_link')
+let home_link = document.getElementById('home_link')
 let aboutme_link = document.getElementById('aboutme_link')
 let skills_link = document.getElementById('skills_link')
 let portfolio_link = document.getElementById('portfolio_link')
@@ -15,40 +16,40 @@ window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     console.clear();
     console.log('scroll', scroll)
-    if(0 < scroll && scroll <  aboutme.offsetTop){
-        home_link.classList.add('active')
-        aboutme_link.classList.remove('active')
-        skills_link.classList.remove('active')
-        portfolio_link.classList.remove('active')
-        contacts_link.classList.remove('active')
+    if (scroll < aboutme.offsetTop) {
+        home_link.classList.add('active_link')
+        aboutme_link.classList.remove('active_link')
+        skills_link.classList.remove('active_link')
+        portfolio_link.classList.remove('active_link')
+        contacts_link.classList.remove('active_link')
         console.log('home')
-    } else if(aboutme.offsetTop < scroll && scroll <  skills.offsetTop){
-        home_link.classList.remove('active')
-        aboutme_link.classList.add('active')
-        skills_link.classList.remove('active')
-        portfolio_link.classList.remove('active')
-        contacts_link.classList.remove('active')
+    } else if (aboutme.offsetTop < scroll && scroll < skills.offsetTop) {
+        home_link.classList.remove('active_link')
+        aboutme_link.classList.add('active_link')
+        skills_link.classList.remove('active_link')
+        portfolio_link.classList.remove('active_link')
+        contacts_link.classList.remove('active_link')
         console.log('aboutme', aboutme.offsetTop)
-    } else if(skills.offsetTop < scroll && scroll <  portfolio.offsetTop){
-        home_link.classList.remove('active')
-        aboutme_link.classList.remove('active')
-        skills_link.classList.add('active')
-        portfolio_link.classList.remove('active')
-        contacts_link.classList.remove('active')
-        console.log('skills', skills.offsetTop)
-    } else if(portfolio.offsetTop < scroll && scroll <  contacts.offsetTop){
-        home_link.classList.remove('active')
-        aboutme_link.classList.remove('active')
-        skills_link.classList.remove('active')
-        portfolio_link.classList.add('active')
-        contacts_link.classList.remove('active')
+    } else if (skills.offsetTop < scroll && scroll < portfolio.offsetTop) {
+        home_link.classList.remove('active_link')
+        aboutme_link.classList.remove('active_link')
+        skills_link.classList.add('active_link')
+        portfolio_link.classList.remove('active_link')
+        contacts_link.classList.remove('active_link')
+        console.log('skills', portfolio.offsetTop)
+    } else if (portfolio.offsetTop < scroll && scroll < contacts.offsetTop) {
+        home_link.classList.remove('active_link')
+        aboutme_link.classList.remove('active_link')
+        skills_link.classList.remove('active_link')
+        portfolio_link.classList.add('active_link')
+        contacts_link.classList.remove('active_link')
         console.log('portfolio', portfolio.offsetTop)
-    } else if(contacts.offsetTop - 150 < scroll){
-        home_link.classList.remove('active')
-        aboutme_link.classList.remove('active')
-        skills_link.classList.remove('active')
-        portfolio_link.classList.remove('active')
-        contacts_link.classList.add('active')
+    } else if (contacts.offsetTop < scroll) {
+        home_link.classList.remove('active_link')
+        aboutme_link.classList.remove('active_link')
+        skills_link.classList.remove('active_link')
+        portfolio_link.classList.remove('active_link')
+        contacts_link.classList.add('active_link')
         console.log('contacts', contacts.offsetTop)
     }
 });
