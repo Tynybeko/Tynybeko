978afec4b584d080ab9ade34f57e38cd0d2889e2
Tynybek <tynybeko1122@gmail.com>
2022-12-13 17:52:04 +0600

    link

---
+++ 10/10.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+});
+
+$(function () {
+  var $videoContainer = $('#video'),
+    $videoControls = $('.video-control'),
+    $video = $('#myVideo')[0];
+
+  $videoControls.click(function () {
+    if ($video.paused) {
+      $video.play();
+      $videoContainer.addClass('video-is-playing');
+    } else {
+      $video.pause();
+      $videoContainer.removeClass('video-is-playing');
+      //  возвращаем постер
+      $video.load();
+    }
+  });
+});
